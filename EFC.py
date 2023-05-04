9e41dd4907e86c00d039c3fb77da7893ffcffdbc
heckeroncrack <124959846+heckeroncrack@users.noreply.github.com>
2023-05-05 00:32:55 +0200

    Update EFC.py

--- EFC.py
+++ EFC.py
@@ -7,7 +7,7 @@ def main(argv):
     opts, args = getopt.getopt(argv,"hi:o:",["ifile=","icofile="])
     for opt, arg in opts:
         if opt == '-h':
-            print ('EFX.py -i <inputfile> -ico <iconfile>')
+            print ('EFC.py -i <inputfile> -ico <iconfile>')
             sys.exit()
         elif opt in ("-i", "--ifile"):
             inputfile = arg
